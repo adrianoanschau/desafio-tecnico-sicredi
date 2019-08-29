fa26d704fcd9307d9854d2e0687f859466fa5591
Adriano Anschau <adrianoanschau@gmail.com>
2019-08-29 20:37:49 -0300

    Criação da entidade 'Associate' juntamente com o Controller, Model, Migration e Factory

---
+++ database/factories/AssociateFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Associate;
+use Faker\Generator as Faker;
+
+$factory->define(Associate::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
