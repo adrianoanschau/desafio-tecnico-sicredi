467889afd288333609a14f934289fd1620995038
Adriano Anschau <adrianoanschau@gmail.com>
2019-08-29 22:35:34 -0300

    Adiciona anotações para métodos dos controllers Associate e Schedule

--- app/Http/Controllers/AssociateController.php
+++ app/Http/Controllers/AssociateController.php
@@ -3,31 +3,54 @@
 namespace App\Http\Controllers;
 
 use App\Models\Associate;
+use Illuminate\Http\JsonResponse;
 
 class AssociateController extends Controller
 {
+    /**
+     * @return JsonResponse
+     */
     public function index()
     {
         return response()->json(Associate::all(), 200);
     }
 
+    /**
+     * @param Associate $associate
+     *
+     * @return JsonResponse
+     */
     public function show(Associate $associate)
     {
         return response()->json($associate, 200);
     }
 
+    /**
+     * @return JsonResponse
+     */
     public function store()
     {
         $associate = Associate::create(request()->all());
         return response()->json($associate, 201);
     }
 
+    /**
+     * @param Associate $associate
+     *
+     * @return JsonResponse
+     */
     public function update(Associate $associate)
     {
         $associate->update(request()->all());
         return response()->json($associate, 200);
     }
 
+    /**
+     * @param Associate $associate
+     *
+     * @return JsonResponse
+     * @throws \Exception
+     */
     public function destroy(Associate $associate)
     {
         $associate->delete();
--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -3,31 +3,54 @@
 namespace App\Http\Controllers;
 
 use App\Models\Schedule;
+use Illuminate\Http\JsonResponse;
 
 class ScheduleController extends Controller
 {
+    /**
+     * @return JsonResponse
+     */
     public function index()
     {
         return response()->json(Schedule::all(), 200);
     }
 
+    /**
+     * @param Schedule $schedule
+     *
+     * @return JsonResponse
+     */
     public function show(Schedule $schedule)
     {
         return response()->json($schedule, 200);
     }
 
+    /**
+     * @return JsonResponse
+     */
     public function store()
     {
         $schedule = Schedule::create(request()->all());
         return response()->json($schedule, 201);
     }
 
+    /**
+     * @param Schedule $schedule
+     *
+     * @return JsonResponse
+     */
     public function update(Schedule $schedule)
     {
         $schedule->update(request()->all());
         return response()->json($schedule, 200);
     }
 
+    /**
+     * @param Schedule $schedule
+     *
+     * @return JsonResponse
+     * @throws \Exception
+     */
     public function destroy(Schedule $schedule)
     {
         $schedule->delete();
